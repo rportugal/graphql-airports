00281ebd8cac969d3090fef74e9475e90a52e263
Ricardo Portugal <ricardofmportugal@gmail.com>
2024-09-22 10:40:28 +0100

    Rewrite - Compare Rust / Node (#1)

---
+++ node/src/index.js
@@ -0,0 +1,85 @@
+const { makeExecutableSchema } = require("graphql-tools");
+const path = require("path");
+const { ApolloServer } = require("@apollo/server");
+
+const typeDefs = `
+type Coordinate {
+  longitude: Float
+  latitude: Float
+  elevation: Float
+}
+
+type Airport {
+  icao: String
+  iata: String
+  name: String
+  city: String
+  state: String
+  country: String
+  coordinate: Coordinate
+  timezone: String
+}
+
+type Query {
+  airports: [Airport]
+}`;
+
+const airports = require(
+  path.join(process.cwd(), "..", "Airports", "airports.json")
+);
+
+const resolvers = {
+  Query: {
+    airports: () => Object.values(airports),
+  },
+  Airport: {
+    coordinate: (obj) => obj,
+    timezone: (obj) => obj.tz,
+  },
+  Coordinate: {
+    longitude: (obj) => obj.lon,
+    latitude: (obj) => obj.lat,
+    elevation: (obj) => obj.elevation,
+  },
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const query = `{
+  airports {
+    icao
+    iata
+    name
+    city
+    state
+    country
+    coordinate {
+      longitude
+      latitude
+      elevation
+    }
+    timezone
+  }
+}`;
+
+const testServer = new ApolloServer({
+  schema,
+});
+
+(async () => {
+  try {
+    const start = Date.now();
+    for (let i = 0; i < 500; i++) {
+      await testServer.executeOperation({
+        query,
+      });
+    }
+    const end = Date.now();
+    console.log("test duration:", (end - start) / 1000, "s");
+  } catch (e) {
+    console.error("something wrong");
+  }
+})();
